a788ea3bef81d6cdfa96072cf80d04b33108c4b0
гансагер «Gans» гансагер <guzhva9999@gmail.com>
2024-11-16 17:57:07 +0200

    added token env

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const { Telegraf } = require('telegraf'); // Используем библиотеку telegraf для Node.js
-const bot = new Telegraf('7814583439:AAGk0LbYWw-UbIDcjgS74HbKICX15RSG9Ck'); // токен вашего бота
+require('dotenv').config();
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN); // токен вашего бота
 
 bot.command('game', (ctx) => {
   ctx.telegram.sendGame(ctx.chat.id, 'frogGame');
